57a69e30ae0c95013fcaa0fe17181789e53f503e
Lomig Mégard <lomig.megard@gmail.com>
2017-01-24 22:15:23 +0100

    Add Java support (#8)

--- akka-http-cors/src/main/scala/ch/megard/akka/http/cors/HttpHeaderRange.scala
+++ akka-http-cors/src/main/scala/ch/megard/akka/http/cors/HttpHeaderRange.scala
@@ -1,11 +1,11 @@
 package ch.megard.akka.http.cors
 
+import java.util.Locale
+
 import scala.collection.immutable.Seq
 
 
-sealed abstract class HttpHeaderRange {
-  def matches(header: String): Boolean
-}
+abstract class HttpHeaderRange extends ch.megard.akka.http.cors.japi.HttpHeaderRange
 
 object HttpHeaderRange {
   case object `*` extends HttpHeaderRange {
@@ -13,8 +13,8 @@ object HttpHeaderRange {
   }
 
   final case class Default(headers: Seq[String]) extends HttpHeaderRange {
-    val lowercaseHeaders = headers.map(_.toLowerCase)
-    def matches(header: String): Boolean = lowercaseHeaders contains header.toLowerCase
+    val lowercaseHeaders = headers.map(_.toLowerCase(Locale.ROOT))
+    def matches(header: String): Boolean = lowercaseHeaders contains header.toLowerCase(Locale.ROOT)
   }
 
   def apply(headers: String*): Default = Default(Seq(headers: _*))
