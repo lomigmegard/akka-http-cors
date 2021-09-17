aedb96c7c6e07f50828cbe6711004a32f9327df5
Scala Steward <me@scala-steward.org>
2021-09-17 18:49:22 +0200

    Reformat with scalafmt 3.0.4

--- akka-http-cors/src/main/scala/ch/megard/akka/http/cors/scaladsl/settings/CorsSettings.scala
+++ akka-http-cors/src/main/scala/ch/megard/akka/http/cors/scaladsl/settings/CorsSettings.scala
@@ -24,84 +24,84 @@ import scala.util.Try
 @DoNotInherit
 abstract class CorsSettings private[akka] () extends javadsl.settings.CorsSettings { self: CorsSettingsImpl =>
 
-  /** If `true`, allow generic requests (that are outside the scope of the specification)
-    * to pass through the directive. Else, strict CORS filtering is applied and any
-    * invalid request will be rejected.
+  /** If `true`, allow generic requests (that are outside the scope of the specification) to pass through the directive.
+    * Else, strict CORS filtering is applied and any invalid request will be rejected.
     *
     * Default: `true`
     */
   def allowGenericHttpRequests: Boolean
 
-  /** Indicates whether the resource supports user credentials.  If `true`, the header
-    * `Access-Control-Allow-Credentials` is set in the response, indicating that the
-    * actual request can include user credentials. Examples of user credentials are:
-    * cookies, HTTP authentication or client-side certificates.
+  /** Indicates whether the resource supports user credentials. If `true`, the header `Access-Control-Allow-Credentials`
+    * is set in the response, indicating that the actual request can include user credentials. Examples of user
+    * credentials are: cookies, HTTP authentication or client-side certificates.
     *
     * Default: `true`
     *
-    * @see [[https://www.w3.org/TR/cors/#access-control-allow-credentials-response-header Access-Control-Allow-Credentials]]
+    * @see
+    *   [[https://www.w3.org/TR/cors/#access-control-allow-credentials-response-header Access-Control-Allow-Credentials]]
     */
   def allowCredentials: Boolean
 
-  /** List of origins that the CORS filter must allow. Can also be set to `*` to allow
-    * access to the resource from any origin. Controls the content of the
-    * `Access-Control-Allow-Origin` response header: if parameter is `*` and credentials
-    * are not allowed, a `*` is set in `Access-Control-Allow-Origin`. Otherwise, the
-    * origins given in the `Origin` request header are echoed.
+  /** List of origins that the CORS filter must allow. Can also be set to `*` to allow access to the resource from any
+    * origin. Controls the content of the `Access-Control-Allow-Origin` response header: if parameter is `*` and
+    * credentials are not allowed, a `*` is set in `Access-Control-Allow-Origin`. Otherwise, the origins given in the
+    * `Origin` request header are echoed.
     *
-    * Hostname starting with `*.` will match any sub-domain.
-    * The scheme and the port are always strictly matched.
+    * Hostname starting with `*.` will match any sub-domain. The scheme and the port are always strictly matched.
     *
-    * The actual or preflight request is rejected if any of the origins from the request
-    * is not allowed.
+    * The actual or preflight request is rejected if any of the origins from the request is not allowed.
     *
     * Default: `HttpOriginMatcher.*`
     *
-    * @see [[https://www.w3.org/TR/cors/#access-control-allow-origin-response-header Access-Control-Allow-Origin]]
+    * @see
+    *   [[https://www.w3.org/TR/cors/#access-control-allow-origin-response-header Access-Control-Allow-Origin]]
     */
   def allowedOrigins: HttpOriginMatcher
 
-  /** List of request headers that can be used when making an actual request. Controls
-    * the content of the `Access-Control-Allow-Headers` header in a preflight response:
-    * if parameter is `*`, the headers from `Access-Control-Request-Headers` are echoed.
-    * Otherwise the parameter list is returned as part of the header.
+  /** List of request headers that can be used when making an actual request. Controls the content of the
+    * `Access-Control-Allow-Headers` header in a preflight response: if parameter is `*`, the headers from
+    * `Access-Control-Request-Headers` are echoed. Otherwise the parameter list is returned as part of the header.
     *
     * Default: `HttpHeaderRange.*`
     *
-    * @see [[https://www.w3.org/TR/cors/#access-control-allow-headers-response-header Access-Control-Allow-Headers]]
+    * @see
+    *   [[https://www.w3.org/TR/cors/#access-control-allow-headers-response-header Access-Control-Allow-Headers]]
     */
   def allowedHeaders: HttpHeaderRange
 
-  /** List of methods that can be used when making an actual request. The list is
-    * returned as part of the `Access-Control-Allow-Methods` preflight response header.
+  /** List of methods that can be used when making an actual request. The list is returned as part of the
+    * `Access-Control-Allow-Methods` preflight response header.
     *
-    * The preflight request will be rejected if the `Access-Control-Request-Method`
-    * header's method is not part of the list.
+    * The preflight request will be rejected if the `Access-Control-Request-Method` header's method is not part of the
+    * list.
     *
     * Default: `Seq(GET, POST, HEAD, OPTIONS)`
     *
-    * @see [[https://www.w3.org/TR/cors/#access-control-allow-methods-response-header Access-Control-Allow-Methods]]
+    * @see
+    *   [[https://www.w3.org/TR/cors/#access-control-allow-methods-response-header Access-Control-Allow-Methods]]
     */
   def allowedMethods: Seq[HttpMethod]
 
-  /** List of headers (other than simple response headers) that browsers are allowed to access.
-    * If not empty, this list is returned as part of the `Access-Control-Expose-Headers`
-    * header in the actual response.
+  /** List of headers (other than simple response headers) that browsers are allowed to access. If not empty, this list
+    * is returned as part of the `Access-Control-Expose-Headers` header in the actual response.
     *
     * Default: `Seq.empty`
     *
-    * @see [[https://www.w3.org/TR/cors/#simple-response-header Simple response headers]]
-    * @see [[https://www.w3.org/TR/cors/#access-control-expose-headers-response-header Access-Control-Expose-Headers]]
+    * @see
+    *   [[https://www.w3.org/TR/cors/#simple-response-header Simple response headers]]
+    * @see
+    *   [[https://www.w3.org/TR/cors/#access-control-expose-headers-response-header Access-Control-Expose-Headers]]
     */
   def exposedHeaders: Seq[String]
 
-  /** When set, the amount of seconds the browser is allowed to cache the results of a preflight request.
-    * This value is returned as part of the `Access-Control-Max-Age` preflight response header.
-    * If `None`, the header is not added to the preflight response.
+  /** When set, the amount of seconds the browser is allowed to cache the results of a preflight request. This value is
+    * returned as part of the `Access-Control-Max-Age` preflight response header. If `None`, the header is not added to
+    * the preflight response.
     *
     * Default: `Some(30 * 60)`
     *
-    * @see [[https://www.w3.org/TR/cors/#access-control-max-age-response-header Access-Control-Max-Age]]
+    * @see
+    *   [[https://www.w3.org/TR/cors/#access-control-max-age-response-header Access-Control-Max-Age]]
     */
   def maxAge: Option[Long]
 
@@ -160,9 +160,8 @@ object CorsSettings {
   def apply(config: Config): CorsSettings =
     fromSubConfig(config.getConfig(prefix))
 
-  /** Creates an instance of settings using the given String of config overrides to override
-    * settings set in the class loader of this class (i.e. by application.conf or reference.conf files in
-    * the class loader of this class).
+  /** Creates an instance of settings using the given String of config overrides to override settings set in the class
+    * loader of this class (i.e. by application.conf or reference.conf files in the class loader of this class).
     */
   def apply(configOverrides: String): CorsSettings =
     apply(ConfigFactory.parseString(configOverrides).withFallback(ConfigFactory.load(getClass.getClassLoader)))
@@ -184,9 +183,8 @@ object CorsSettings {
     */
   implicit def default(implicit system: ActorSystem): CorsSettings = apply(system)
 
-  /** Settings from the default loaded configuration.
-    * Note that application code may want to use the `apply()` methods instead
-    * to have more control over the source of the configuration.
+  /** Settings from the default loaded configuration. Note that application code may want to use the `apply()` methods
+    * instead to have more control over the source of the configuration.
     */
   def defaultSettings: CorsSettings = apply(ConfigFactory.load(getClass.getClassLoader))
 
